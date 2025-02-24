876f5180a9852e0c4982c45b21bb21a51dd55b5f
Edynu <eduardoc.422005@gmail.com>
2025-02-24 14:20:41 -0300

    Remove merge conflict markers and cleanup conflict resolution

--- utils/file_transform.py
+++ utils/file_transform.py
@@ -9,10 +9,6 @@ def combine_dotf_files(input_folder, output_file):
                     if file.endswith('.dotf'):
                         file_path = os.path.join(root, file)
                         if not os.path.commonprefix([os.path.realpath(file_path), os.path.realpath(input_folder)]) == os.path.realpath(input_folder):
-<<<<<<< HEAD
-=======
-                            print(f"Arquivo {file_path} está fora do diretório permitido.")
->>>>>>> d33347e54785bdf917f3ee1caf18af3b22d1f604
                             continue
                         with open(file_path, 'rb') as infile:
                             data = infile.read()
@@ -21,19 +17,11 @@ def combine_dotf_files(input_folder, output_file):
                             outfile.write(b'\0')
                             outfile.write(data)
                         index.write(f"{file}\t{os.path.basename(file_path)}\n")
-<<<<<<< HEAD
-=======
-    print(f"Todos os arquivos .dotf foram combinados em {output_file} e o índice foi salvo em {index_file}")
->>>>>>> d33347e54785bdf917f3ee1caf18af3b22d1f604
 
 def split_dotf_file(input_file, output_folder):
     index_file = os.path.join(os.path.dirname(input_file), 'index.txt')
     if not os.path.exists(index_file):
-<<<<<<< HEAD
         raise FileNotFoundError(f"Index file not found: {index_file}")
-=======
-        raise FileNotFoundError(f"Arquivo de índice não encontrado: {index_file}")
->>>>>>> d33347e54785bdf917f3ee1caf18af3b22d1f604
     
     with open(index_file, 'r') as index:
         index_data = {line.split('\t')[0]: line.split('\t')[1].strip() for line in index.readlines()}
@@ -55,14 +43,6 @@ def split_dotf_file(input_file, output_folder):
             original_filename = index_data.get(filename, filename)
             output_path = os.path.join(output_folder, original_filename)
             if not os.path.commonprefix([os.path.realpath(output_path), os.path.realpath(output_folder)]) == os.path.realpath(output_folder):
-<<<<<<< HEAD
                 continue
             with open(output_path, 'wb') as outfile:
                 outfile.write(data)
-=======
-                print(f"Caminho de saída {output_path} está fora do diretório permitido.")
-                continue
-            with open(output_path, 'wb') as outfile:
-                outfile.write(data)
-    print(f"Arquivo {input_file} foi separado em arquivos individuais na pasta {output_folder} e os nomes originais foram restaurados")
->>>>>>> d33347e54785bdf917f3ee1caf18af3b22d1f604
